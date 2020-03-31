824751797304f98f3411bc5263e80ede08004cee
Léo <leo.lebrasf@gmail.com>
2020-03-31 13:27:37 +0200

    :sparkles: Make the project ready to start

---
+++ src/theme.js
@@ -0,0 +1,7 @@
+export const primaryColor50 = '#63fA87'
+
+export const neutralColor00 = '#ffffff'
+export const neutralColor50 = '#6b6565'
+export const neutralColor100 = '#0a0206'
+
+export const fontFace = 'Proxima Nova'
